e80d2f04a4c750e1ea6d43e0060665e8e056ff58
bt <btfranzen@gmail.com>
2021-06-09 13:59:33 -0600

    init

---
+++ understanding_typescript.js
@@ -0,0 +1,72 @@
+1 GETTING STARTED
+- ts is a js superset - builds on js but adds new features and advantages
+- ts cannot be executed by the browser or node
+- environments where we can execute js do not support ts
+- ts is a programming language but also a tool - powerful compiler that runs over your code to compile ts code to js
+- ts --> js
+- ts adds new features via the compiler
+- adds types to the js lanaguage that gives you the opportunity to type things.
+- extra error checking
+- any time you access the value of an input element in js, it is always a string.
+- can convert a variable to a number by add + in front of the variable
+- ! means the variable will never be null
+- :type (example -- :number) typecasts to this type 
+
+
+2 TYPESCRIPT BASICS & BASIC TYPES
+
+
+
+3 THE TYPESCRIPT COMPILER (& ITS CONFIGURATION)
+
+
+
+4 NEXT-GENERATION JAVASCRIPT & TYPESCRIPT
+
+
+
+5 CLASSES & INTERFACES
+
+
+
+6 ADVANCED TYPES
+
+
+
+7 GENERICS
+
+
+
+8 DECORATORS
+
+
+
+9 BUILD A DRAG & DROP PROJECT
+
+
+
+10 MODULES & NAMESPACES
+
+
+
+11 USING WEBPACK WITH TYPESCRIPT
+
+
+
+12 3RD PARTY LIBRARIES & TYPESCRIPT
+
+
+
+13 SELECT & SHARE A PLACE PROJECT
+
+
+
+14 REACT & TYPESCRIPT
+
+
+
+15 NODE + EXPRESS & TYPESCRIPT
+
+
+
+16 COURSE ROUNDUP
