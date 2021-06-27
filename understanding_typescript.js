31a7c45b9ded45363e6eb50eedad6d196ac2957d
bt <btfranzen@gmail.com>
2021-06-27 09:29:50 -0600

    added project setup, section 1 z

--- understanding_typescript.js
+++ understanding_typescript.js
@@ -18,7 +18,71 @@
     - meta-programming features like decorators
     - rich configuration options
     - modern tooling that helps even in non-ts projects
-    
+
+- project setup
+    - project folder
+        - index.html
+            - html:5 in vscode will give you boilerplate
+            - add script tag in head
+                <script src="app.js" defer></script>
+        - app.ts
+            - typescript file
+            - to compile in vs code terminal
+                    tsc app.js
+                - kicks off the logic from app.ts in a new app.js file
+
+    - can double click the html file in the finder window to open in the browser to see changers
+
+    - installing lite-server to avoid manual reloads/recompiles:
+        - install npm in the project
+            npm init
+            - setup config file (can just accept all defaults by hitting enter for each)
+        - open package.json file created by npm init command
+        - install lite-server
+            npm install --save-dev lite-server
+        - when install completes:
+            - open package.json file
+            - in the "scripts" object, add a comma at the end of the "test" line
+            - add "start": "lite-server" to the object
+
+            should now look like this:
+            {
+              "name": "project_setup",
+              "version": "1.0.0",
+              "description": "",
+              "main": "app.js",
+              "scripts": {
+                "test": "echo \"Error: no test specified\" && exit 1",
+                "start": "lite-server"
+              },
+              "author": "",
+              "license": "ISC",
+              "devDependencies": {
+                "lite-server": "^2.6.1"
+              }
+            }
+
+    - using lite-server
+        - in the terminal, start by using
+            npm start
+
+        - make changes
+        - recompile
+            tsc app.ts
+        - see changes at http://localhost:3000/
+
+    - stopping lite-server
+        - in the terminal window, hit ctrl + c
+
+    - lite-server
+        - simple development server which always servers the index.html file on localhost:3000
+        - can go to http://localhost:3000/ and see code
+        - updates anytime any file in the directory updates
+        - saves us from reloading to see changes (but still must recompile with tsc app.ts)
+
+
+
+
 
 
 2 TYPESCRIPT BASICS & BASIC TYPES
