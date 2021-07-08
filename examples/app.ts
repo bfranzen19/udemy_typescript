4d0440e1c6c35e91be8da8b4319ab5e9d767529c
bt <btfranzen@gmail.com>
2021-07-08 09:53:16 -0600

    type assignment & type inference

--- examples/app.ts
+++ examples/app.ts
@@ -1,14 +1,39 @@
-function add(n1: number, n2: number) {
-    /* js way of doing things *//*
-    if(typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('incorect input!');
-    }*/
 
-    return n1 + n2;
+
+
+
+// ------------------------------------------------------------
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    const result = n1+n2;
+
+    if(showResult) {
+        console.log(phrase + result);
+    } else {
+        return result;
+    }
 }
 
-const number1 = "5";
+const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = `result is: `;
 
-const result = add(number1, number2);
-console.log('result: ', result);
+add(number1, number2, printResult, resultPhrase);
+
+// ------------------------------------------------------------
+
+// function add(n1: number, n2: number) {
+//     /* js check for type */
+//     if(typeof n1 !== 'number' || typeof n2 !== 'number') {
+//         throw new Error('incorect input!');
+//     }
+
+//     return n1 + n2;
+// }
+
+// const number1 = 5; /* "5" */
+// const number2 = 2.8;
+
+// const result = add(number1, number2);
+// console.log('result: ', result);
